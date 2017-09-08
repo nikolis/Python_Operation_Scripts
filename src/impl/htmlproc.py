2c7c611cde8286d810dc8ef654d57c136e14a523
nikolisgal <nikolisgal@gmail.com>
2017-09-08 10:53:50 +0200

    Clean first Child function

--- src/impl/htmlproc.py
+++ src/impl/htmlproc.py
@@ -8,12 +8,15 @@ import base64
 escapeCharacters =['½','¼','¾','¼']
 escapeCharsMatcher ={'½':0.5, '¼':0.25, '¾':0.75}
 
-base64encoded_points =[]
+base64encoded_points =set()
 multiplyer = 'x'
 rangeOperator = '-'
 measurement_units=["small","big","leaves","clove","cloves","oz","tsp","tbsp","kg","fl","ml","bunch","slices","rashers","g","lb","jar"]
 
-def _findIngredientsTag(soup):
+def _find_ingredients_tag(htmlDoc):
+    """This method is responsible for geting an html Document and finding the div object
+    that belongs to the CSS class 'recipe-ingredients'"""
+    soup = BeautifulSoup(htmlDoc, 'html.parser')
     tags = soup.find_all('div')
     for tag in tags:
         retTags =[]
@@ -21,63 +24,78 @@ def _findIngredientsTag(soup):
         if((not attributes is None) and('class' in attributes.keys()) and (tag['class']==['recipe-ingredients'])):
             return tag
 
-def _listIngredients(ingredientsTag):
+def _list_ingredients(ingredientsTag):
+    """This method gets as a parameter the complete Div object that found to belong 
+    in the 'recipe-ingredients' class"""
+    recipeIngredients = {}
     stringIngredientsTag = str(ingredientsTag) 
     soapTemp = BeautifulSoup(stringIngredientsTag,'html.parser')
     header_tags =soapTemp.find_all(re.compile('h\d'))
     list_tags =soapTemp.find_all("ul")
+    loopCounter = 0 
     for listTag in list_tags:
-        _extract_ingredients_from_list(str(listTag))
+        ingredientEntries =  _extract_ingredients_from_list(str(listTag))
+        recipeIngredients.update({header_tags[loopCounter].text:ingredientEntries})
+        loopCounter+=1
+    return recipeIngredients
 
 def _extract_ingredients_from_list(ulist):
+    """This method gets as a parameter a <ul> html object whis should contain the
+    ingredients we need to parse and save"""
+    ingredientEntries = []
     ulist = __encode_escape_chars(ulist)
     soapTemp = BeautifulSoup(ulist,'html.parser')
     listItems = soapTemp.find_all('li')
     for item in listItems:
         children = item.children 
-        get_ingredient_from_children(children)
-        print ("-------")
-        stringItem = str(item)
-
+        ingredientEntry = get_ingredient_from_children(children)
+        ingredientEntries.append(ingredientEntry)
+    return ingredientEntries 
 
 def get_ingredient_from_children(children):
     childNum = 1
     measurementUnit =None
-    furtherDesc =None
+    pre_object_description =None
     quant =-1
     for child in children:
         child = _get_rid_of_backslashes(child.string)        
         if (childNum ==1):
-            ingredientEntry = find_combined_quantity(child)
-            print(ingredientEntry)
-            quant = ingredientEntry[0]
-            if(ingredientEntry[1]==None and ingredientEntry[2]):
-                for mo in measurement_units:
-                    loopCount =0
-                    descrTokens = ingredientEntry[2].split(" ")
-                    for token in descrTokens:
-                        if (token == mo):
-                            measurementUnit=mo
-                            furtherDesc = " ".join(descrTokens[0:loopCount] + descrTokens[loopCount+1:])
-                        loopCount+=1
-                if(measurementUnit):
-                    furtherDesc = ingredientEntry[2].replace(measurementUnit,"")
-                else :
-                    furtherDesc = ingredientEntry[2]
-            else :
-                measurementUnit = ingredientEntry[1]
-                furtherDesc = ingredientEntry[2]
+            result = handle_first_child(child)
+            quant, measurementUnit, pre_object_description = result[0],result[1],result[2]
         childNum+=1
-    print(quant,measurementUnit,furtherDesc)
-    return (quant,measurementUnit,furtherDesc)
+    return (quant,measurementUnit,pre_object_description)
 
-"""
-def handle_first_child:
+
+def handle_first_child(firstChild):
+    measurementUnit =None
+    furtherDesc =None
+    quant =-1
+    
+    ingredientEntry = find_combined_quantity(firstChild)
+    quant = ingredientEntry[0]
+    if(ingredientEntry[1]==None and ingredientEntry[2]):
+        for mo in measurement_units:
+            loopCount =0
+            descrTokens = ingredientEntry[2].split(" ")
+            for token in descrTokens:
+                if (token == mo):
+                    measurementUnit=mo
+                    furtherDesc = " ".join(descrTokens[0:loopCount] + descrTokens[loopCount+1:])
+                loopCount+=1
+            if(measurementUnit):
+                furtherDesc = ingredientEntry[2].replace(measurementUnit,"")
+            else :
+                furtherDesc = ingredientEntry[2]
+    else:
+        measurementUnit = ingredientEntry[1]
+        furtherDesc = ingredientEntry[2]
+    return (quant,measurementUnit,furtherDesc)
 
 def handle_second_child:
 
+ """
 def handle_third_child:
-"""
+    """
 
 def find_combined_quantity(raw):
     """ Return a tuple that has 3 members the 
@@ -120,11 +138,11 @@ def find_combined_quantity(raw):
                 remainingDescriptions.append(token)
         loopCounter+=1
     return (combinedNumber,None, " ".join(remainingDescriptions))   
-                
-            
-                 
-            
-               
+
+
+
+
+
 def is_number(token):
     """String:input, Tuple:output this function gets a string token as parameter
     and return a tuple containing either a number and and None if there is nothing more
@@ -158,7 +176,7 @@ def __encode_escape_chars(string):
         index = tempString.find(ch)
         while(index != -1):
             newString = tempString[:index]+str(base64.b64encode(bytes(tempString[index:index+len(ch)],'utf-8')))+tempString[index+len(ch):]
-            base64encoded_points.append(str(base64.b64encode(bytes(tempString[index:index+len(ch)], 'utf-8'))))
+            base64encoded_points.add(str(base64.b64encode(bytes(tempString[index:index+len(ch)], 'utf-8'))))
             tempString = newString
             index = tempString.find(ch)
     return tempString
@@ -199,19 +217,7 @@ recipe_urls = ['http://www.bbc.co.uk/food/recipes/aged_sirloin_steak_with_62354'
 
 url = recipe_urls[0];
 htmlDoc = urlopen(url).read()
-soup = BeautifulSoup(htmlDoc, 'html.parser')
-#soup = BeautifulSoup(markup, 'html.parser')
-#script_tag = soup.script
-
-#while(script_tag):
-#script_tag.decompose()
-#script_tag = soup.script
-
-#soup.head.decompose()
-#print (soup.prettify())
-ingredient_tag = _findIngredientsTag(soup)
-ingredientList = _listIngredients(ingredient_tag)
-#print(" 4 x 300g/10wr0=oz aged")
-#print(get_rid_of_backslashes(" 4 x 300g/10wr0=oz aged"))
-print (ingredientList)
-
+ingredient_tag = _find_ingredients_tag(htmlDoc)
+ingredientList = _list_ingredients(ingredient_tag)
+for entity in ingredientList:
+    print(entity," : ",ingredientList.get(entity))
