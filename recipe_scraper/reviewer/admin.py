5c003f5e92b52e9660842530e29993678f72d7ec
nikolis <nikolisgal@gmail.com>
2017-09-26 20:42:57 +0200

    removing django app

--- recipe_scraper/reviewer/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Ingredient
-from .models import Step
-from .models import Recipe
-
-admin.site.register(Ingredient)
-admin.site.register(Step)
-admin.site.register(Recipe)
